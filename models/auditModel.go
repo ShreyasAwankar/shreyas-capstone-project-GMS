bf220f25ec49d152c137fe2a14525bfcbf43e78e
ShreyasAwankar <shreyasawankar01@gmail.com>
2024-02-06 19:21:23 +0530

    first commit

--- models/auditModel.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type AuditRecord struct {
-	Action     string      `json:"Action"`
-	DocumentID string      `json:"DocumentId"`
-	Timestamp  interface{} `json:"Timestamp"`
-	UserID     string      `json:"UserId"`
-}
